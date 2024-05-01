74667fffda9c17d1ac4d2f555b539bf629d110e1
Neko020P <144085002+Neko020P@users.noreply.github.com>
2024-05-01 11:39:40 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './src/routes/userRoutes.js.old';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/CRMdb');
+
+// For parsing application/json
+app.use(express.json());
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// JWT setup
+app.use((req, res, next) => {
+    if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+        jwt.verify(req.headers.authorization.split(' ')[1], process.env.JWT_SECRET, (err, decode) => {
+            if (err) req.user = undefined;
+            req.user = decode;
+            next();
+        });
+    } else {
+        req.user = undefined;
+        next();
+    }
+});
+
+// Routes
+routes(app);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) => 
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, '0.0.0.0', () => 
+    console.log(`Your server is running on port ${PORT}`)
+);
